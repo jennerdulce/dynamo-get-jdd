d345f706f9e491ceec45f96fb74688782b844459
Jenner Dulce <jennerdulce@gmail.com>
2021-04-21 21:33:14 -0700

    changes

--- index.js
+++ index.js
@@ -6,12 +6,28 @@ const clientsModel = require('./schema.js');
 exports.handler = async (event) => {
   console.log('EVENT', event)
   try {
-    const list = await clientsModel.scan().exec()
-    const data = JSON.stringify(list)
-    return {
-      statusCode: 201,
-      body: data
+    if (!event.body) {
+      const list = await clientsModel.scan().exec()
+      const data = JSON.stringify(list)
+      return {
+        statusCode: 201,
+        body: data
+      }
+    } else {
+      const body = JSON.parse(event.body)
+      if (body.id) {
+        let user = await clientsModel.get({ "id": body.id })
+      } else if (body.name) {
+        let user = await clientsModel.get({ "name": body.name })
+      } else if (body.phone) {
+        let user = await clientsModel.get({ "phone": body.phone })
+      }
+      return {
+        statusCode: 201,
+        body: user
+      }
     }
+
   } catch (e) {
     return {
       statusCode: 500,
